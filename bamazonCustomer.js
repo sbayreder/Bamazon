ac242ce81911f714222ceeb952624478c4846f50
sbayreder <sbayreder@gmail.com>
2020-04-17 15:33:54 -0500

    finish first objective all code working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,102 +3,106 @@ var inquirer = require("inquirer");
 var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Mlpftw13!",
-    database: "bamazon",
+  host: "localhost",
+  user: "root",
+  password: "Mlpftw13!",
+  database: "bamazon",
+  port: 3306
 });
-console.log(connection);
-connection.connect(function (err) {
+
+connection.connect();
+
+var display = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    runSearch();
-});
-var display = function () {
-    connection.query("SELECT * FROM products", function (err, res) {
+    console.log("-----------------------------");
+    console.log("      Welcome To Bamazon    ");
+    console.log("-----------------------------");
+    console.log("");
+    console.log("Find below our Products List");
+    console.log("");
+    var table = new Table({
+      head: ["Item Id", "Product Name","Department Name", "Price", "Stock"],
+      colWidths: [12, 40, 20, 12, 8],
+      colAligns: ["center", "left", "right"],
+      style: {
+        head: ["aqua"],
+        compact: true
+        // 'padding-right' : 1,
+      }
+    });
+
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].products_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+    }
+
+    console.log(table.toString());
+    console.log("");
+    shopping();
+  }); //End Connection to products
+};
+
+var shopping = function() {
+  inquirer
+    .prompt({
+      name: "productToBuy",
+      type: "input",
+      message: "Please enter the Product Id of the item you wish to purchase.!"
+    })
+    .then(function(answer1) {
+      var selection = answer1.productToBuy;
+      connection.query("SELECT * FROM products WHERE item_Id=?", selection, function(
+        err,
+        res
+      ) {
         if (err) throw err;
-        console.log("============================");
-        console.log("     -Welcome To Bamazon-     ");
-        console.log("============================");
-        console.log("");
-        console.log("Our Products List!");
-        console.log("");
-        var table = new Table({
-            head: ["Product Id", "Product Description", "Cost"],
-            colWidths: [12, 50, 8],
-            colAligns: ["center", "left", "right"],
-            style: {
-                head: ["aqua"],
-                compact: true,
-            },
-        });
-        for (var i = 0; i < res.length; i++) {
-            table.push([res[i].id, res[i].products_name, res[i].price]);
+        if (res.length === 0) {
+          console.log(
+            "That Product doesn't exist, Please enter a Product Id from the list above"
+          );
+
+          shopping();
+        } else {
+          inquirer
+            .prompt({
+              name: "quantity",
+              type: "input",
+              message: "How many items woul you like to purchase?"
+            })
+            .then(function(answer2) {
+              var quantity = answer2.quantity;
+              if (quantity > res[0].stock_quantity) {
+                console.log(
+                  "Our Apologies we only have " +
+                    res[0].stock_quantity +
+                    " items of the product selected"
+                );
+                shopping();
+              } else {
+                console.log("");
+                console.log(res[0].products_name + " purchased");
+                console.log(quantity + " qty @ $" + res[0].price);
+
+                var newQuantity = res[0].stock_quantity - quantity;
+                connection.query(
+                  "UPDATE products SET stock_quantity = " +
+                    newQuantity +
+                    " WHERE item_id = " +
+                    res[0].item_id,
+                  function(err, resUpdate) {
+                    if (err) throw err;
+                    console.log("");
+                    console.log("Your Order has been Processed");
+                    console.log("Thank you for Shopping with us...!");
+                    console.log("");
+                    connection.end();
+                  }
+                );
+              }
+            });
         }
-        console.log(table.toString());
-        console.log("");
-        shopping();
+      });
     });
 };
-var shopping = function () {
-    inquirer
-        .prompt({
-            name: "productToBuy",
-            type: "input",
-            message: "Please Enter The Product Id of The Item You Wish To Purchase!",
-        })
-        .then(function (answer1) {
-            var selection = answer1.productToBuy;
-            connection.query(
-                "SELECT * FROM products WHERE Id=?",
-                selection,
-                function (err, res) {
-                    if (err) throw err;
-                    if (res.length === 0) {
-                        console.log(
-                            "That Product Doesn't Exist, Please Enter A Product Id From The List Above!"
-                        );
-                        shopping();
-                    } else {
-                        inquirer
-                            .prompt({
-                                name: "quantity",
-                                type: "input",
-                                message: "How Many Items Would You Like To Purchase?",
-                            })
-                            .then(function (answer2) {
-                                var quantity = answer2.quantity;
-                                if (quantity > res[0].stock_quantity) {
-                                    console.log(
-                                        "Our Apologies We Only Have " +
-                                        res[0].stock_quantity +
-                                        " Items of The Product Selected"
-                                    );
-                                    shopping();
-                                } else {
-                                    console.log("");
-                                    console.log(res[0].products_name + " purchased");
-                                    console.log(quantity + " qty @ $" + res[0].price);
-                                    var newQuantity = res[0].stock_quantity - quantity;
-                                    connection.query(
-                                        "UPDATE products SET stock_quantity = " +
-                                        newQuantity +
-                                        " WHERE id = " +
-                                        res[0].id,
-                                        function (err, resUpdate) {
-                                            if (err) throw err;
-                                            console.log("");
-                                            console.log("Your Order Has Been Processed!");
-                                            console.log("Thank You For Shopping With Bamazon!");
-                                            console.log("");
-                                            connection.end();
-                                        }
-                                    );
-                                }
-                            });
-                    }
-                }
-            );
-        });
-};
+
 display();
